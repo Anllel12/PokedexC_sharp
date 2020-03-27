a76a6f45078d554fddfd499bf95522334ac55c9c
Angel <anllelonair@gmail.com>
2020-03-27 22:00:51 +0100

    Realizada la conexion a la Base de Datos, ya pone las imagenes y las nombres de cada pokemon

--- PokedexC_sharp/VentanaPrincipal.Designer.cs
+++ PokedexC_sharp/VentanaPrincipal.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.imagen = new System.Windows.Forms.PictureBox();
+            this.anterior = new System.Windows.Forms.Button();
+            this.siguiente = new System.Windows.Forms.Button();
+            this.nombre = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagen
+            // 
+            this.imagen.Location = new System.Drawing.Point(13, 13);
+            this.imagen.Name = "imagen";
+            this.imagen.Size = new System.Drawing.Size(239, 228);
+            this.imagen.TabIndex = 0;
+            this.imagen.TabStop = false;
+            // 
+            // anterior
+            // 
+            this.anterior.Location = new System.Drawing.Point(13, 267);
+            this.anterior.Name = "anterior";
+            this.anterior.Size = new System.Drawing.Size(75, 23);
+            this.anterior.TabIndex = 1;
+            this.anterior.Text = "<";
+            this.anterior.UseVisualStyleBackColor = true;
+            this.anterior.Click += new System.EventHandler(this.anterior_Click);
+            // 
+            // siguiente
+            // 
+            this.siguiente.Location = new System.Drawing.Point(177, 267);
+            this.siguiente.Name = "siguiente";
+            this.siguiente.Size = new System.Drawing.Size(75, 23);
+            this.siguiente.TabIndex = 2;
+            this.siguiente.Text = ">";
+            this.siguiente.UseVisualStyleBackColor = true;
+            this.siguiente.Click += new System.EventHandler(this.siguiente_Click);
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(259, 13);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(100, 23);
+            this.nombre.TabIndex = 3;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.siguiente);
+            this.Controls.Add(this.anterior);
+            this.Controls.Add(this.imagen);
+            this.Name = "VentanaPrincipal";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imagen;
+        private System.Windows.Forms.Button anterior;
+        private System.Windows.Forms.Button siguiente;
+        private System.Windows.Forms.Label nombre;
     }
 }
 
--- PokedexC_sharp/VentanaPrincipal.cs
+++ PokedexC_sharp/VentanaPrincipal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,43 @@ namespace PokedexC_sharp
 {
     public partial class VentanaPrincipal : Form
     {
+        Conexion conexion= new Conexion();
+        DataTable pokemons = new DataTable();
+
+        int idActual = 1;//el pokemon que estamos viendo
+
         public VentanaPrincipal()
         {
             InitializeComponent();
+        }
+
+        private Image blobImagen(byte[] img)
+        {
+            MemoryStream ms = new System.IO.MemoryStream(img);//lee el formato de la Base de Datos y lo transforma en imagen
+            return (Image.FromStream(ms));
+        }
+
+
+        private void anterior_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            if (idActual <= 0) { idActual = 1; }
+
+            pokemons = conexion.getPokemonId(idActual);//pasa el id a la clase Conexion
+
+            nombre.Text = pokemons.Rows[0]["nombre"].ToString();//coge la columna nombre y la pasa a String
+            imagen.Image = blobImagen((byte[])pokemons.Rows[0]["imagen"]);//pone la imagen del pokemon
+        }
+
+        private void siguiente_Click(object sender, EventArgs e)
+        {
+            idActual++;
+            if (idActual >= 151) { idActual = 151; }
+
+            pokemons = conexion.getPokemonId(idActual);//pasa el id a la clase Conexion
+
+            nombre.Text = pokemons.Rows[0]["nombre"].ToString();//coge la columna nombre y la pasa a String
+            imagen.Image = blobImagen((byte[])pokemons.Rows[0]["imagen"]);//pone la imagen del pokemon
         }
     }
 }
