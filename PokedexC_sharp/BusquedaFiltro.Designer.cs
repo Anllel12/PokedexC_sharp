c90772b35ba8d0642ff5dfe04c80d91e9c94e363
Angel <anllelonair@gmail.com>
2020-04-04 19:10:57 +0200

    Añadidas todas las opciones de filtro

--- PokedexC_sharp/BusquedaFiltro.Designer.cs
+++ PokedexC_sharp/BusquedaFiltro.Designer.cs
@@ -28,44 +28,316 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BusquedaFiltro));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.filtrar = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // checkedListBox1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(1, 99);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(798, 352);
-            this.dataGridView1.TabIndex = 0;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Semilla",
+            "Lagartija",
+            "Llama",
+            "Tortuguita",
+            "Tortuga",
+            "Marisco",
+            "Gusano",
+            "Capullo",
+            "Mariposa",
+            "Oruga",
+            "Abeja Asesina",
+            "Pajarito",
+            "Pajaro",
+            "Pico",
+            "Serpiente",
+            "Cobra",
+            "Raton",
+            "Pin Veneno",
+            "Taladro",
+            "Hada",
+            "Zorro",
+            "Globo",
+            "Murcielago",
+            "Hierbajo",
+            "Hongo",
+            "Insecto",
+            "Polilla Venenosa",
+            "Topo",
+            "Gato Araña",
+            "Gato Fino",
+            "Pato",
+            "Mono Cerdo",
+            "Perrito",
+            "Legendario",
+            "Renacuajo",
+            "Psi",
+            "Superpoder",
+            "Flor",
+            "Matamoscas",
+            "Medusa",
+            "Roca",
+            "Megaton",
+            "Caballo Fuego",
+            "Atontado",
+            "Ermitaño",
+            "Iman",
+            "Pato Salvaje",
+            "Ave Gemela",
+            "Ave Triple",
+            "Leon Marino",
+            "Lodo",
+            "Bivalvo",
+            "Gas",
+            "Sombra",
+            "Serpiente Roca",
+            "Hipnosis",
+            "Cangrejo",
+            "Tenaza",
+            "Bola",
+            "Huevo",
+            "Coco",
+            "Solitario",
+            "Apilahueso",
+            "Patada",
+            "Puñetazo",
+            "Lametazo",
+            "Gas Veneno",
+            "Clavos",
+            "Huevo",
+            "Enredadera",
+            "Padres",
+            "Pez Color",
+            "Misterioso",
+            "Barrera",
+            "Mantis",
+            "Forma Humana",
+            "Electrico",
+            "Escupe Fuego",
+            "Escarabajo",
+            "Toro Bravo",
+            "Pez",
+            "Atrocidad",
+            "Transporte",
+            "Transformacion",
+            "Evolucion",
+            "Burbuja",
+            "Relampago",
+            "Virtual",
+            "Espiral",
+            "Marisco",
+            "Fosil",
+            "Dormilon",
+            "Congelar",
+            "Dragon",
+            "Genetico",
+            "Nueva Especie"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 62);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(140, 244);
+            this.checkedListBox1.TabIndex = 2;
             // 
-            // textBox1
+            // filtrar
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
-            this.textBox1.TabIndex = 1;
+            this.filtrar.BackColor = System.Drawing.Color.Transparent;
+            this.filtrar.FlatAppearance.BorderSize = 0;
+            this.filtrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.filtrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.filtrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.filtrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filtrar.Location = new System.Drawing.Point(596, 12);
+            this.filtrar.Name = "filtrar";
+            this.filtrar.Size = new System.Drawing.Size(98, 50);
+            this.filtrar.TabIndex = 20;
+            this.filtrar.Text = "Filtrar";
+            this.filtrar.UseVisualStyleBackColor = false;
+            this.filtrar.Click += new System.EventHandler(this.filtrar_Click);
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.Color.Black;
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(140, 50);
+            this.label.TabIndex = 21;
+            this.label.Text = "Especie";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(158, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 50);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Tipo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(450, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 50);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Habitat";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(304, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 50);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Habilidad";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Agua",
+            "Bicho",
+            "Dragon",
+            "Electrico",
+            "Fantasma",
+            "Fuego",
+            "Hielo",
+            "Lucha",
+            "Normal",
+            "Planta",
+            "Psiquico",
+            "Roca",
+            "Tierra",
+            "Veneno",
+            "Volador"});
+            this.checkedListBox2.Location = new System.Drawing.Point(158, 62);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(140, 244);
+            this.checkedListBox2.TabIndex = 26;
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "Aborbe agua",
+            "Absor. elec.",
+            "Absorbe Agua",
+            "Absorbe Fuego",
+            "Aclimatacion",
+            "Agallas",
+            "Armad.Bat",
+            "Cabeza Roca",
+            "Caparazon",
+            "Clorofila",
+            "Corte Fuerte",
+            "Cuerpo Llama",
+            "Cuerpo Maldito",
+            "Cuerpo Puro",
+            "Cura Natural",
+            "Despiste",
+            "Electricidad Estatica",
+            "Enjambre",
+            "Espesura",
+            "Espiritu Vital",
+            "Flexibilidad",
+            "Foco Interno",
+            "Fuga",
+            "Gran Encanto",
+            "Hedor",
+            "Humedad",
+            "Iman",
+            "Insomnio",
+            "Insonorizar",
+            "Intimidacion",
+            "Levitacion",
+            "Madrugar",
+            "Mar llamas",
+            "Mudar",
+            "Nado Rapido",
+            "OjoCompuesto",
+            "Pararrayos",
+            "Polvo Escudo",
+            "Presion",
+            "Punto Toxico",
+            "Rastro",
+            "Recogida",
+            "Sebo",
+            "Sincronia",
+            "Torrente",
+            "Velo Arena",
+            "Vista Lince"});
+            this.checkedListBox3.Location = new System.Drawing.Point(304, 62);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(140, 244);
+            this.checkedListBox3.TabIndex = 27;
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Items.AddRange(new object[] {
+            "Agua Dulce",
+            "Agua Salada",
+            "Bosque",
+            "Campo",
+            "Caverna",
+            "Ciudad",
+            "Montaña",
+            "Pradera",
+            "RARO"});
+            this.checkedListBox4.Location = new System.Drawing.Point(450, 62);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(140, 244);
+            this.checkedListBox4.TabIndex = 28;
             // 
             // BusquedaFiltro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(706, 314);
+            this.Controls.Add(this.checkedListBox4);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.filtrar);
+            this.Controls.Add(this.checkedListBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "BusquedaFiltro";
             this.Text = "BusquedaFiltro";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button filtrar;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
     }
 }
--- PokedexC_sharp/BusquedaFiltro.cs
+++ PokedexC_sharp/BusquedaFiltro.cs
@@ -16,5 +16,10 @@ namespace PokedexC_sharp
         {
             InitializeComponent();
         }
+
+        private void filtrar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
